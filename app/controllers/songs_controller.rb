280f106b14b4862101910f345b0e5cd4876d449b
github username <github email address>
2020-01-20 16:38:21 -0500

    add changes to fix one error out of many more

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,16 +6,20 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    #@song = Song.create(params[:song][:name], params[:artist_id])
+    @artists = Artist.all
+  #  @artists = Genre.all
+    erb :"songs/new"
+  end
+
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     #binding.pry
     erb :'/songs/show'
   end
 
-  get '/songs/new' do
-  #  @song = Song.create(params[:song][:name], params[:artist_id])
-    erb :"songs/new"
-  end
 
   # post '/song' do
   #   @artist = Artist.create(params["name"])
